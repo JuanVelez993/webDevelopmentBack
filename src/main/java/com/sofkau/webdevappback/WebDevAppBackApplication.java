1e20eb1b1f910fc96202e4b2b12cd7afd943b48f
Juan Pablo Velez <juan.velez993@gmail.com>
2022-05-04 09:24:17 -0500

    Initial proyect layout

---
+++ src/main/java/com/sofkau/webdevappback/WebDevAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.webdevappback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDevAppBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkau/webdevappback/WebDevAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkau.webdevappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDevAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
